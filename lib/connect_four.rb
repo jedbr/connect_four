bdafaa747be5e28cb5792b32c2e266a41896b540
jedrzej bryll <jedrzej.bryll@gmail.com>
2016-11-14 18:09:52 +0100

    Tweak #play and add #winner? methods

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,18 +1,20 @@
 class ConnectFour
   def initialize
-    @board = Array.new(6) { Array.new(7) }
-    @winner = nil
-    @current_player = "●"
+    @board = Array.new(7) { Array.new(6) }
+    @current_player = "● "
   end
 
   def play
-    until @winner || board_full?
+    until winner? || board_full?
       switch_players
       print_board
       next_move
-      winner_check
     end
     finish_game
+  end
+
+  def winner?
+
   end
 
   def board_full?
@@ -24,32 +26,34 @@ class ConnectFour
   end
 
   def switch_players
-    @current_player = @current_player == "○" ? "●" : "○"
+    @current_player = @current_player == "○ " ? "● " : "○ "
   end
 
   def print_board
-    @board.each do |column|
-      puts column.join("|")
+    (0..5).reverse_each do |i|
+      to_print = []
+      @board.each do |column|
+        to_print << column[i]
+      end
+      puts to_print.join(" | ")
     end
   end
 
   def next_move
-    puts "What your next move, #{@current_player} ?"
     loop do
-      i = gets.chomp
-      if i.to_i.between?(1, 7)
-        make_move
+      puts "What your next move, #{@current_player} ?"
+      i = gets.chomp.to_i
+      if i.between?(1, 7) && !@board[i - 1].all?
+        make_move(i - 1)
         break
       end
+      puts "Invalid move. Try again."
     end
   end
 
-  def make_move
-
-  end
-
-  def winner_check
-
+  def make_move(column)
+    row = @board[column].find_index(nil)
+    @board[column][row] = @current_player
   end
 
   def finish_game
